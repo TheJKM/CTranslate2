29e16548b416bc8ed99edf3fc7abb98b6f06c1e3
Johannes Kreutz <jkm@jkm-marburg.de>
2023-10-26 22:56:39 +0000

    Port to ROCm: first batch

--- src/cuda/random.h
+++ src/cuda/random.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <curand_kernel.h>
+#include <hiprand_kernel.h>
 
 namespace ctranslate2 {
   namespace cuda {
 
-    curandStatePhilox4_32_10_t* get_curand_states(size_t num_states);
+    hiprandStatePhilox4_32_10_t* get_curand_states(size_t num_states);
 
   }
 }
